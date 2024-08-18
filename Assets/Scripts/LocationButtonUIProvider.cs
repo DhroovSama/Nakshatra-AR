958f471766ef5ef7a16697fd8315080c2151debf
Dhroov Sharma <mailmeatdhroovsharma@gmail.com>
2024-08-18 20:36:16 +0530

    Collectibles Trigger System 1.4v

--- Assets/Scripts/LocationButtonUIProvider.cs
+++ Assets/Scripts/LocationButtonUIProvider.cs
@@ -8,11 +8,13 @@ public class LocationButtonUIProvider : MonoBehaviour
     [SerializeField]
     private List<RawImage> locationUIList = new List<RawImage>();
 
+    [Space]
     [SerializeField]
     private List<GameObject> loactionUIListContainGameobjects = new List<GameObject>();
 
-    //[SerializeField]
-    //private List<GameObject> collectiblesGameobjects = new List<GameObject>();
+    [Space]
+    [SerializeField]
+    private List<GameObject> collectible_1_Element, collectible_2_LunarSurfaceImages, collectible_3_TempratureData, collectible_4_TopographicalData, collectible_5_SouthPole;
 
     public List<RawImage> GetLocationUIList()
     {
@@ -24,8 +26,28 @@ public class LocationButtonUIProvider : MonoBehaviour
         return loactionUIListContainGameobjects;
     }
 
-    //public List<GameObject> GetCollectiblesGameobjects()
-    //{
-    //    return collectiblesGameobjects;
-    //}
+    public List<GameObject> GetCollectible_1_Element()
+    {
+        return collectible_1_Element;
+    }
+
+    public List<GameObject> GetCollectible_2_LunarSurfaceImages()
+    {
+        return collectible_2_LunarSurfaceImages;
+    }
+
+    public List<GameObject> GetCollectible_3_TempratureDatas()
+    {
+        return collectible_3_TempratureData;
+    }
+
+    public List<GameObject> GetCollectible_4_TopographicalData()
+    {
+        return collectible_4_TopographicalData;
+    }
+
+    public List<GameObject> GetCollectible_5_SouthPole()
+    {
+        return collectible_5_SouthPole;
+    }
 }
--- Assets/Scripts/TriggerCollectibleThroughRaycast.cs
+++ Assets/Scripts/TriggerCollectibleThroughRaycast.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,38 +7,82 @@ public class TriggerCollectibleThroughRaycast : MonoBehaviour
 {
     public static TriggerCollectibleThroughRaycast Instance { get; private set; }
 
+    [Header("Vibration Settings")]
+    [Tooltip("Reference to the VibrationController SO for handling haptic feedback.")]
     [SerializeField]
-    private VibrationController vibration;  // Reference to the VibrationController
+    private VibrationController vibration;
 
+    [SerializeField]
+    private LocationButtonUIProvider locationButtonUIProvider;
+
+    [Header("UI Elements")]
+    [Tooltip("Progress bar UI element to show collectible progress.")]
     [SerializeField]
     private GameObject collectiblesProgressBarGameobject;
 
+    [Tooltip("Slider for the collectible progress bar.")]
     [SerializeField]
     private Slider collectiblesProgressBar;
 
+    [Header("Raycast Settings")]
+    [Tooltip("The range of the raycast for detecting collectibles.")]
     [SerializeField]
     private float raycastRange = 10f;
 
+    [Tooltip("Duration to fill the progress bar.")]
     [SerializeField]
     private float fillDuration = 2f;
 
+    [Tooltip("Delay before resetting the progress bar.")]
     [SerializeField]
     private float resetDelay = 0.5f;
 
+    [Tooltip("Duration for the location reveal UI to be visible.")]
     [SerializeField]
     private float durationForLoactionRevealUI = 3f;
 
+    [Header("References to Other Scripts")]
+    [Tooltip("Reference to the TriggerTerrainScanner script.")]
     [SerializeField]
     private TriggerTerrainScanner triggerTerrainScanner;
 
+    [Tooltip("Button to enable the terrain scanner.")]
     [SerializeField]
     private Button enableTerrainScannerButton;
 
+    [Tooltip("Reference to the CollectibleManager script.")]
     [SerializeField]
     private CollectibleManager collectibleManager;
 
+    [Header("Voice Over Data")]
+    [Tooltip("VoiceOverData for Element VO")]
+    [SerializeField]
+    private VoiceOverData Collectible_1_vo;
+
+    [Tooltip("VoiceOverData for Lunar Surface Image VO")]
+    [SerializeField]
+    private VoiceOverData Collectible_2_vo;
+
+    [Tooltip("VoiceOverData for Temprature Data VO")]
+    [SerializeField]
+    private VoiceOverData Collectible_3_vo;
+
+    [Tooltip("VoiceOverData for Topographical Data VO")]
+    [SerializeField]
+    private VoiceOverData Collectible_4_vo;
+
+    [Tooltip("VoiceOverData for South Pole VO")]
+    [SerializeField]
+    private VoiceOverData Collectible_5_vo;
+
+    [Space(10)]
+    [Tooltip("The current collectible targetted by the raycast.")]
     [SerializeField]
     public GameObject CurrentTargetCollectible;
+
+    [Space]
+    [SerializeField]
+    private List<GameObject> collectible_1_Element, collectible_2_LunarSurfaceImages, collectible_3_TempratureData, collectible_4_TopographicalData, collectible_5_SouthPole;
 
     private bool isFilling = false;
     private bool shouldRaycast = false;
@@ -69,6 +114,17 @@ public class TriggerCollectibleThroughRaycast : MonoBehaviour
         if (PlaceOnPlane.IsMoonSurfaceSpawned())
         {
             collectibleManager = FindObjectOfType<CollectibleManager>();
+
+            locationButtonUIProvider = FindObjectOfType<LocationButtonUIProvider>();
+
+            if(locationButtonUIProvider != null )
+            {
+                collectible_1_Element = locationButtonUIProvider.GetCollectible_1_Element();
+                collectible_2_LunarSurfaceImages = locationButtonUIProvider.GetCollectible_2_LunarSurfaceImages();
+                collectible_3_TempratureData = locationButtonUIProvider.GetCollectible_3_TempratureDatas();
+                collectible_4_TopographicalData = locationButtonUIProvider.GetCollectible_4_TopographicalData();
+                collectible_5_SouthPole = locationButtonUIProvider.GetCollectible_5_SouthPole();
+            }
         }
 
         if (shouldRaycast)
@@ -163,6 +219,91 @@ public class TriggerCollectibleThroughRaycast : MonoBehaviour
                tag == "Collectible 4" || tag == "Collectible 5";
     }
 
+    private void HandleCollectibleInteraction(GameObject collectible)
+    {
+        switch (collectible.tag)
+        {
+            case "Collectible 1":
+                HandleCollectible_1_Element();
+                break;
+            case "Collectible 2":
+                HandleCollectible_2_LunarSurfaceImage();
+                break;
+            case "Collectible 3":
+                HandleCollectible_3_TempratureData();
+                break;
+            case "Collectible 4":
+                HandleCollectible_4_TopographicalData();
+                break;
+            case "Collectible 5":
+                HandleCollectible_5_SouthPole();
+                break;
+            default:
+                Debug.LogWarning("Unknown collectible tag: " + collectible.tag);
+                break;
+        }
+    }
+
+    private void HandleCollectible_1_Element()
+    {
+        Debug.Log("Handling interaction for Collectible 1");
+
+        VoiceOverManager.Instance.TriggerVoiceOver(Collectible_1_vo);
+
+        foreach(GameObject collectibles in collectible_1_Element)
+        {
+            collectibles.SetActive(true);
+        }
+    }
+
+    private void HandleCollectible_2_LunarSurfaceImage()
+    {
+        Debug.Log("Handling interaction for Collectible 2");
+
+        VoiceOverManager.Instance.TriggerVoiceOver(Collectible_2_vo);
+
+        foreach (GameObject collectibles in collectible_2_LunarSurfaceImages)
+        {
+            collectibles.SetActive(true);
+        }
+    }
+
+    private void HandleCollectible_3_TempratureData()
+    {
+        Debug.Log("Handling interaction for Collectible 3");
+
+        VoiceOverManager.Instance.TriggerVoiceOver(Collectible_3_vo);
+
+        foreach (GameObject collectibles in collectible_3_TempratureData)
+        {
+            collectibles.SetActive(true);
+        }
+    }
+
+    private void HandleCollectible_4_TopographicalData()
+    {
+        Debug.Log("Handling interaction for Collectible 4");
+
+        VoiceOverManager.Instance.TriggerVoiceOver(Collectible_4_vo);
+
+        foreach (GameObject collectibles in collectible_4_TopographicalData)
+        {
+            collectibles.SetActive(true);
+        }
+    }
+
+    private void HandleCollectible_5_SouthPole()
+    {
+        Debug.Log("Handling interaction for Collectible 5");
+
+        VoiceOverManager.Instance.TriggerVoiceOver(Collectible_5_vo);
+
+        foreach (GameObject collectibles in collectible_5_SouthPole)
+        {
+            collectibles.SetActive(true);
+        }
+    }
+
     private IEnumerator FillCollectiblesProgressBar(GameObject collectible)
     {
         isFilling = true;
@@ -189,6 +330,9 @@ public class TriggerCollectibleThroughRaycast : MonoBehaviour
 
         // Set the target collectible in TriggerTerrainScanner
         triggerTerrainScanner.targetCollectible = collectible;
+
+        // Handle the specific collectible interaction
+        HandleCollectibleInteraction(collectible);
 
         // Disable all other collectibles except the current target
         DisableOtherCollectibles(collectible);
